bb6ae21a5faef93ab99e873d63a8ced91ebaeca9
baniex <banjibaniex1@gmail.com>
2023-09-13 13:05:52 +0100

    hello

--- src/components/Twitter/Twitter.jsx
+++ src/components/Twitter/Twitter.jsx
@@ -1,13 +1,49 @@
 import api from "../../api"
-
+import { useEffect } from "react"
 
 const Twitter = () => {
+
+  useEffect(()=>{
+
+
+    const urlParams = new URLSearchParams(window.location.search);
+    //  const oauthTokenParam = urlParams.get("oauth_token");
+    //  const oauthVerifierParam = urlParams.get("oauth_verifier");
+
+    const oauthTokenParam = "john snow"
+    const oauthVerifierParam = 'bad boy z'
+    console.log("hi")
+
+
+     if(oauthTokenParam && oauthVerifierParam){
+
+      console.log("second")
+        try {
+          api.post("/send-twitter-token-and-verifier",{
+            oauthTokenParam, oauthVerifierParam
+
+          })
+          
+        } catch (error) {
+          console.log(error);
+          
+        }
+
+     }
+
+
+  }, [])
 
 
   const connectToTwitter = async () => {
 
     const getRequestToken = await api.get("/get-twitter-request-token")
-    console.log(getRequestToken)
+    console.log(getRequestToken);
+    if(getRequestToken){
+      let oauth_token = getRequestToken.data.serverData.oauth_token;
+      window.location.replace(`https://api.twitter.com/oauth/authorize?oauth_token=${oauth_token}`);
+
+    }
 
   }
 
@@ -19,3 +55,4 @@ const Twitter = () => {
 }
 
 export default Twitter
+

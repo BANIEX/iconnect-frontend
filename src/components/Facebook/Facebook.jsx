5092e71a5a15572744a83bf0dd6a2b00ecb3ca8e
baniex <banjibaniex1@gmail.com>
2023-10-03 20:45:13 +0100

    hello

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -1,25 +1,35 @@
-import FacebookLogin from '@greatsumini/react-facebook-login';
+// import FacebookLogin from '@greatsumini/react-facebook-login';
 
-
+import FacebookLogin from 'react-facebook-login';
 
 
 const Facebook = () => {
 
+  const responseFacebook = (response) => {
+  console.log(response);
+}
+
   
 
   return (
-    <FacebookLogin
-      appId="1336749817261894"
-      onSuccess={(response) => {
-        console.log("Login Success!", response);
-      }}
-      onFail={(error) => {
-        console.log("Login Failed!", error);
-      }}
-      onProfileSuccess={(response) => {
-        console.log("Get Profile Success!", response);
-      }}
-    />
+    // <FacebookLogin
+    //   appId=""
+    //   onSuccess={(response) => {
+    //     console.log("Login Success!", response);
+    //   }}
+    //   onFail={(error) => {
+    //     console.log("Login Failed!", error);
+    //   }}
+    //   onProfileSuccess={(response) => {
+    //     console.log("Get Profile Success!", response);
+    //   }}
+    // />
+     <FacebookLogin
+    appId="1336749817261894"
+    autoLoad={true}
+    fields="name,email,picture"
+    onClick={componentClicked}
+    callback={responseFacebook} />,
   );
 }
 

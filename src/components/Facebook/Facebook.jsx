f59f009da897b386087678a99f0dce067a55dc2f
baniex <banjibaniex1@gmail.com>
2023-10-05 17:44:24 +0100

    j1

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -1,19 +1,20 @@
-// import FacebookLogin from '@greatsumini/react-facebook-login';
+import FacebookLogin from '@greatsumini/react-facebook-login';
 
-import FacebookLogin from 'react-facebook-login';
+// import FacebookLogin from 'react-facebook-login';
 
 
 const Facebook = () => {
 
-  const responseFacebook = (response) => {
-  console.log(response);
-}
+//   const responseFacebook = (response) => {
+//   console.log(response);
+// }
 
   
 
   return (
+
     // <FacebookLogin
-    //   appId=""
+    //   appId="1336749817261894"
     //   onSuccess={(response) => {
     //     console.log("Login Success!", response);
     //   }}
@@ -24,12 +25,31 @@ const Facebook = () => {
     //     console.log("Get Profile Success!", response);
     //   }}
     // />
-     <FacebookLogin
-    appId="1336749817261894"
-    autoLoad={true}
-    fields="name,email,picture"
-    onClick={componentClicked}
-    callback={responseFacebook} />,
+
+    // <FacebookLogin
+    //   appId="1336749817261894"
+    //   autoLoad={true}
+    //   fields="name,email,picture"
+    //   // onClick={componentClicked}
+    //   callback={responseFacebook}
+    // />
+
+    <FacebookLogin
+      appId="1336749817261894"
+      onSuccess={(response) => {
+        console.log("Login Success!", response);
+      }}
+      onFail={(error) => {
+        console.log("Login Failed!", error);
+      }}
+      onProfileSuccess={(response) => {
+        console.log("Get Profile Success!", response);
+      }}
+
+      // render={({ onClick, logout }) => (
+      //   <CustomComponent onClick={onClick} onLogoutClick={logout} />
+      // )}
+    />
   );
 }
 
